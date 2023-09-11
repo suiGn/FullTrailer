7a3b65b014c0e02641290818f57e9aea3f67b8e3
Sui Gn <natsynapses@gmail.com>
2023-09-11 11:00:39 -0500

    ..
    ..

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/suign/Desktop/me/@apps/fullTrailer/apps/webapp/src/app/page.js
+// File: /Users/suign/Desktop/me/@apps/fullTrailer/src/app/page.js
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
